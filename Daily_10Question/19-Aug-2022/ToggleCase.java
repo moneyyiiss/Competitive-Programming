bc14c1c6d7870f34ccd8684cd36046af383ccaf7
Manish <moneyyiiss@gmail.com>
2022-08-19 14:50:20 +0530

    ToggleCase

---
+++ Daily_10Question/19-Aug-2022/ToggleCase.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class ToggleCase{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        System.out.println(toggleCase(str));
+    }
+    public static String toggleCase(String str){
+        StringBuilder sb = new StringBuilder(str);
+        for(int i=0; i<sb.length(); i++){
+            char ch = sb.charAt(i);
+
+            if(ch >= 'a' && ch<= 'z'){
+                char uc = (char)('A' + ch - 'a');
+                sb.setCharAt(i,uc);
+            }else{
+                char lc = (char)('a' + ch - 'A');
+                sb.setCharAt(i, lc);
+            }
+        }
+        return sb.toString();
+    }
+}
