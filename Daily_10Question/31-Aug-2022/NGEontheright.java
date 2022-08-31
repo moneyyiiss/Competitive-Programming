52bd06321460ca5653ec8766f921375419b3cae4
Manish <moneyyiiss@gmail.com>
2022-08-31 18:12:34 +0530

    update

--- Daily_10Question/31-Aug-2022/NGEontheright.java
+++ Daily_10Question/31-Aug-2022/NGEontheright.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main {
+public class NGEontheright {
     public static void display(int[] a){
         StringBuilder sb = new StringBuilder();
         for(int val: a){
